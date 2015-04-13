f93b95ccc37358d7cd2206ed83919d0056e95ace
Romain de Wolff <rdewolff@gmail.com>
2015-04-13 10:54:10 +0200

    Added extra check that game is not left without knowing

--- client/init.js
+++ client/init.js
@@ -1,10 +1,15 @@
 Meteor.startup(function() {
 
-	// initial page load 
+	// initial page load
 	Session.set('page', 'home');
 
 	// add missing class and id for template JS to work properly
 	$('body').addClass('svg');
 	$('body').attr('id', 'page-top');
+
+	// avoid quit game without knowing
+	window.onbeforeunload = function() {
+		return "Voulez-vous vraiment quitter?";
+	};
 
 });
