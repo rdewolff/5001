311859793fc7c013fc832f2510ef0eb450178ac9
Romain de Wolff <rdewolff@gmail.com>
2015-06-10 15:52:23 +0200

    Added test - change username

--- tests/mocha/client/sampleClientTest.js
+++ tests/mocha/client/sampleClientTest.js
@@ -1,5 +1,6 @@
 if (!(typeof MochaWeb === 'undefined')){
   MochaWeb.testOnly(function(){
+
     describe("a group of tests", function(){
       it("should respect equality", function(){
         chai.assert.equal(5,5);
@@ -9,11 +10,22 @@ if (!(typeof MochaWeb === 'undefined')){
     describe("login as guest", function() {
     	it("login as guest and check default username", function() {
 	    	$('#guest').click();
+        // gives the time to change pages
 	    	setTimeout(function() {
 					var defaultGuestName = $('#name.form-control').val();
 	    		chai.assert(defaultGuestName == "Yoshi", "Default guest username is not Yoshi");
 	    	}, 50);
     	});
+    });
+
+    describe('change username', function() {
+      it('choose username Toto and continue', function() {
+        $('#name').val('Toto');
+        setTimeout(function() {
+          // chai.assert
+          chai.assert($('h5').text().indexOf('Yoshi') != -1, "Username not changed correctly");
+        }, 50);
+      });
     });
   });
 }
--- tests/mocha/server/sampleServerTest.js
+++ tests/mocha/server/sampleServerTest.js
@@ -1,6 +1,6 @@
 if (!(typeof MochaWeb === 'undefined')){
   MochaWeb.testOnly(function(){
-    describe("Server initialization", function(){
+    describe("server initialization", function(){
       it("should have a Meteor version defined", function(){
         chai.assert(Meteor.release);
       });
